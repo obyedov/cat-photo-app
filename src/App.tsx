230a18097c4bc48b8eef5eb52d707e949d4139fa
Vitaliy Obyedov <obyedov@gmail.com>
2025-02-19 22:36:17 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <main className="App">
+            <h1>CatPhotoApp</h1>
+            <h2>Cat Photos</h2>
+            {/* <!--TODO: Add link to cat photos-->*/}
+            <p>Everyone loves cute cats online!</p>
+        </main>
+    </>
+  )
+}
+
+export default App
