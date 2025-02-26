31338d255ecd21726a723a5a19c15ef5529302b9
Vitaliy Obyedov <obyedov@gmail.com>
2025-02-26 13:15:42 +0100

    Add two section elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,12 +9,15 @@ function App() {
     <>
         <main className="App">
             <h1>CatPhotoApp</h1>
-            <h2>Cat Photos</h2>
-            <p>Everyone loves <a href="https://cdn.freecodecamp.org/curriculum/cat-photo-app/running-cats.jpg">cute cats</a> online!</p>
-            <p>
-                <span>See more</span> <a href="https://freecatphotoapp.com" target="_blank">cat photos</a> <span>in our gallery.</span>
-            </p>
-            {/*<img src=<img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg" alt="A cute orange cat lying on its back">*/}
+            <section>
+                <h2>Cat Photos</h2>
+                <p>Everyone loves <a href="https://cdn.freecodecamp.org/curriculum/cat-photo-app/running-cats.jpg">cute cats</a> online!</p>
+                <p>
+                    <span>See more</span> <a href="https://freecatphotoapp.com" target="_blank">cat photos</a> <span>in our gallery.</span>
+                </p>
+                {/*<img src=<img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg" alt="A cute orange cat lying on its back">*/}
+            </section>
+            <section></section>
         </main>
     </>
   )
