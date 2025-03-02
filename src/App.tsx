5da446779000645b02acf109930bc0b146e284e6
Vitaliy Obyedov <obyedov@gmail.com>
2025-03-02 14:00:13 +0100

    Add list an image of second section

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,33 @@ import './App.css'
 
 function App() {
 
-  return (
-    <>
-        <main className="App">
-            <h1>CatPhotoApp</h1>
-            <section>
-                <h2>Cat Photos</h2>
-                <p>Everyone loves <a href="https://cdn.freecodecamp.org/curriculum/cat-photo-app/running-cats.jpg">cute cats</a> online!</p>
-                <p>
-                    <span>See more</span> <a href="https://freecatphotoapp.com" target="_blank">cat photos</a> <span>in our gallery.</span>
-                </p>
-                {/*<img src=<img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg" alt="A cute orange cat lying on its back">*/}
-            </section>
-            <section>
-                <h2>Cat Lists</h2>
-                <h3>Things cats love:</h3>
-                <ul>
-                    <li>cat nip</li>
-                    <li>laser pointers</li>
-                    <li>lasagna</li>
-                </ul>
-            </section>
-        </main>
-    </>
-  )
+    return (
+        <>
+            <main className="App">
+                <h1>CatPhotoApp</h1>
+                <section>
+                    <h2>Cat Photos</h2>
+                    <p>Everyone loves <a href="https://cdn.freecodecamp.org/curriculum/cat-photo-app/running-cats.jpg">cute
+                        cats</a> online!</p>
+                    <p>
+                        <span>See more</span> <a href="https://freecatphotoapp.com" target="_blank">cat photos</a>
+                        <span>in our gallery.</span>
+                    </p>
+                    {/*<img src=<img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg" alt="A cute orange cat lying on its back">*/}
+                </section>
+                <section>
+                    <h2>Cat Lists</h2>
+                    <h3>Things cats love:</h3>
+                    <ul>
+                        <li>cat nip</li>
+                        <li>laser pointers</li>
+                        <li>lasagna</li>
+                    </ul>
+                    <img src={"https://cdn.freecodecamp.org/curriculum/cat-photo-app/lasagna.jpg"} alt="A slice of lasagna on a plate." />
+                </section>
+            </main>
+        </>
+    )
 }
 
 export default App
