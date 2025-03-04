1b65e68b8a7cefc90ad72ca0c3054be853f7af5e
Vitaliy Obyedov <obyedov@gmail.com>
2025-03-04 22:27:46 +0100

    Add new third section

--- src/App.tsx
+++ src/App.tsx
@@ -29,8 +29,9 @@ function App() {
                     </ul>
                     <figure>
                         <img src={"https://cdn.freecodecamp.org/curriculum/cat-photo-app/lasagna.jpg"} alt="A slice of lasagna on a plate." />
-                        <figcaption>A cute cat</figcaption>
+                        <figcaption>Cats <em>love</em> lasagna</figcaption>
                     </figure>
+                    <h3>Top 3 things cats hate:</h3>
                 </section>
             </main>
         </>
