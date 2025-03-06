a65a532ee844b5b0886f5435dc7e177eecbc8a11
Vitaliy Obyedov <obyedov@gmail.com>
2025-03-06 22:24:28 +0100

    finish with main

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,8 @@ function App() {
                         <li>lasagna</li>
                     </ul>
                     <figure>
-                        <img src={"https://cdn.freecodecamp.org/curriculum/cat-photo-app/lasagna.jpg"} alt="A slice of lasagna on a plate." />
+                        <img src={"https://cdn.freecodecamp.org/curriculum/cat-photo-app/lasagna.jpg"}
+                             alt="A slice of lasagna on a plate."/>
                         <figcaption>Cats <em>love</em> lasagna</figcaption>
                     </figure>
                     <h3>Top 3 things cats hate:</h3>
@@ -37,7 +38,11 @@ function App() {
                         <li>thunder</li>
                         <li>other cats</li>
                     </ol>
-                    <figure> </figure>
+                    <figure>
+                        <img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/cats.jpg"
+                             alt="Five cats looking around a field."/>
+                        <figcaption>Cats <strong>hate</strong> other cats.</figcaption>
+                    </figure>
                 </section>
             </main>
         </>
